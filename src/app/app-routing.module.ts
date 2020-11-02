d4c64427f29a1bd90a59138c8f297527f000bcd5
PadmaGnanapriya <padmaisuru@gmail.com>
2020-11-02 14:20:49 +0530

    refactor code-  change file hierarchy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginAndSignUpComponent} from './dashboard/root/login-and-sign-up/login-and-sign-up.component';
+import {LoginAndSignUpComponent} from './root/login-and-sign-up/login-and-sign-up.component';
 
 const routes: Routes = [
   {path: '', component: LoginAndSignUpComponent}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginAndSignUpComponent } from './dashboard/root/login-and-sign-up/login-and-sign-up.component';
+import { LoginAndSignUpComponent } from './root/login-and-sign-up/login-and-sign-up.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {MatTabsModule} from '@angular/material/tabs';
 
